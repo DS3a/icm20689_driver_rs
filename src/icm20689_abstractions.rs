783fffb606ff9ac9192396e933894ab2f3b7b4b1
DS3a <33351122+DS3a@users.noreply.github.com>
2023-01-13 03:16:22 +0530

    add abstractions with registers and enums for actions

---
+++ src/icm20689_abstractions.rs
@@ -0,0 +1,46 @@
+#![allow(dead_code)]
+macro_rules! register {
+    ($name:ident, $address:expr) => {
+        pub const $name: u8 = $address;
+    }
+}
+
+const g: f64 = 9.807;
+const I2C_RATE: u32 = 400_000;
+
+pub enum GyroConfig {
+    GYRO_RANGE_250DPS = 0x00,
+    GYRO_RANGE_500DPS = 0x08,
+    GYRO_RANGE_1000DPS = 0x10,
+    GYRO_RANGE_2000DPS = 0x18,
+}
+
+pub enum AccelConfig {
+    ACCEL_RANGE_2G = 0x00,
+    ACCEL_RANGE_4G = 0x08,
+    ACCEL_RANGE_8G = 0x10,
+    ACCEL_RANGE_16G = 0x18,
+}
+
+pub mod configuration {
+    register!(GYRO_CONFIG, 0x1b);
+    register!(ACCEL_CONFIG, 0x1c);
+}
+
+pub mod gyroscope_measurements {
+    register!(GYRO_XOUT_H, 0x43);
+    register!(GYRO_XOUT_L, 0x44);
+    register!(GYRO_YOUT_H, 0x45);
+    register!(GYRO_YOUT_L, 0x46);
+    register!(GYRO_ZOUT_H, 0x47);
+    register!(GYRO_ZOUT_L, 0x48);
+}
+
+pub mod accelerometer_measurements {
+    register!(ACCEL_XOUT_H, 0x3b);
+    register!(ACCEL_XOUT_L, 0x3c);
+    register!(ACCEL_YOUT_H, 0x3d);
+    register!(ACCEL_YOUT_L, 0x3e);
+    register!(ACCEL_ZOUT_H, 0x3f);
+    register!(ACCEL_ZOUT_L, 0x40);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+#![allow(dead_code)]
+use embedded_hal::blocking::{delay::DelayMs, i2c};
+
+mod icm20689_abstractions;
+use icm20689_abstractions::accelerometer_measurements;
+
+pub struct ICM20689<I>
+where
+    I: i2c::Write + i2c::Read,
+{
+    i2c: I,
+    address: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<I> ICM20689<I> where I: i2c::Write + i2c::Read {
+    pub fn new(i2c: I, address: u8) -> Self {
+        Self {
+            i2c,
+            address,
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn init(&self) {
+        // TODO check documentation pg 48
+        // set Power_management_1(0x6B) to choose the best clock source
+        // reset the icm20689
+        // wait for reset (delay)
+        // set power mgmt_1 to choose the best clock source again
+        // enable the sensor (write 0x00 to pwr_mgmt_2 (0x6C))
+
+        // select accelerometer range
+        // select gyroscope range
+        // select accelerometer and gyroscope scale based on the selected range
+
+        // TODO study the low pass filter
     }
 }
